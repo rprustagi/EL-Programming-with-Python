417524f0b2394361ee56af5b5aaa9683c17eebd4
Ram Rustagi <rprustagi@smartron.com>
2019-04-19 12:17:20 +0530

    Lecture 04: Loops

--- Sample-Programs/poly-n-star.py
+++ Sample-Programs/poly-n-star.py
@@ -1,15 +1,17 @@
 from turtle import *
-
-x=200
-n=11
+import sys
+sys.argv = ["polystar", 11]
+x=100
+n=int(sys.argv[1])
 outangle = (360/n)
 polyangle = 180 - outangle
 firstangle = outangle/2
-otherangle = 180 - polyangle/(n-2)
-
+#otherangle = 180 - polyangle/(n-2) # lowermost point
+onepartangle = polyangle/(n-2) #highermost point
+turnangle = firstangle + outangle + onepartangle
 left(firstangle)
 for i in range(n):
   forward(x)
-  left(otherangle)
+  left(turnangle)
 
 input()
